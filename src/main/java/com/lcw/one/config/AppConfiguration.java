658702195e926dd979e753b1f34ffc695bdebd3d
Lcw <lcw2004@163.com>
2016-07-01 22:44:59 +0800

    1. 添加对JSP的支持 2. 添加热部署的配置

---
+++ src/main/java/com/lcw/one/config/AppConfiguration.java
@@ -0,0 +1,9 @@
+package com.lcw.one.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfiguration {
+
+
+}
---
+++ src/main/java/com/lcw/one/controll/IndexController.java
@@ -0,0 +1,14 @@
+package com.lcw.one.controll;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "test";
+    }
+
+}
