2e739553f66985eee4b1abe7135b3617110d3696
李昌文 <lcw2004@163.com>
2016-09-18 21:45:52 +0800

    添加RoleService

---
+++ src/main/java/com/lcw/one/modules/sys/service/RoleService.java
@@ -0,0 +1,43 @@
+package com.lcw.one.modules.sys.service;
+
+import com.lcw.one.common.persistence.Page;
+import com.lcw.one.modules.sys.dao.RoleDao;
+import com.lcw.one.modules.sys.entity.Dict;
+import com.lcw.one.modules.sys.entity.Role;
+import com.lcw.one.modules.sys.utils.UserUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+public class RoleService {
+
+    @Autowired
+    private RoleDao roleDao;
+
+    public Page<Role> find(Page<Role> page) {
+        return roleDao.find(page);
+    }
+
+    public Role get(String id) {
+        return roleDao.get(id);
+    }
+
+    public List<Role> findAll() {
+        return UserUtils.getRoleList();
+    }
+
+    @Transactional(readOnly = false)
+    public void save(Role role) {
+        roleDao.clear();
+        roleDao.save(role);
+    }
+
+    @Transactional(readOnly = false)
+    public void delete(String id) {
+        roleDao.deleteById(id);
+    }
+}
