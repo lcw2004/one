e18e262d942d7270f9e549c280bb9b3aac394783
李昌文 <lcw2004@163.com>
2016-10-24 22:27:51 +0800

    删除无用内容

--- sys/src/main/java/com/lcw/one/common/persistence/BaseEntity.java
+++ sys/src/main/java/com/lcw/one/common/persistence/BaseEntity.java
@@ -5,116 +5,17 @@
  */
 package com.lcw.one.common.persistence;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.google.common.collect.Maps;
-import com.lcw.one.modules.sys.entity.User;
-import com.lcw.one.modules.sys.utils.UserUtils;
 
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Transient;
-import javax.xml.bind.annotation.XmlTransient;
 import java.io.Serializable;
-import java.util.Map;
 
 /**
  * Entity支持类
  * @author ThinkGem
  * @version 2013-01-15
  */
-@MappedSuperclass
 public abstract class BaseEntity<T> implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * 当前用户
-	 */
-	protected User currentUser;
-	
-	/**
-	 * 当前实体分页对象
-	 */
-	protected Page<T> page;
-
-	/**
-	 * 自定义SQL（SQL标识，SQL内容）
-	 */
-	protected Map<String, String> sqlMap;
-
-	/**
-	 *从页面搜索还是从菜单搜索（用于非页面搜索时设置默认搜索条件）
-	 */
-	private boolean searchFromPage;
-
-	/**
-	 *用于搜索多个ID的时候设置搜索条件
-	 */
-	private String ids;
-	
-	@JsonIgnore
-	@XmlTransient
-	@Transient
-	public User getCurrentUser() {
-		if(currentUser == null){
-			currentUser = UserUtils.getUser();
-		}
-		return currentUser;
-	}
-	
-	public void setCurrentUser(User currentUser) {
-		this.currentUser = currentUser;
-	}
-
-	@JsonIgnore
-	@XmlTransient
-	@Transient
-	public Page<T> getPage() {
-		if (page == null){
-			page = new Page<T>();
-		}
-		return page;
-	}
-	
-	public Page<T> setPage(Page<T> page) {
-		this.page = page;
-		return page;
-	}
-
-	@JsonIgnore
-	@XmlTransient
-	@Transient
-	public Map<String, String> getSqlMap() {
-		if (sqlMap == null){
-			sqlMap = Maps.newHashMap();
-		}
-		return sqlMap;
-	}
-
-	public void setSqlMap(Map<String, String> sqlMap) {
-		this.sqlMap = sqlMap;
-	}
-	
-	@Transient
-	public boolean isSearchFromPage() {
-		return searchFromPage;
-	}
-
-	@Transient
-	public void setSearchFromPage(boolean searchFromPage) {
-		this.searchFromPage = searchFromPage;
-	}
-
-	@Transient
-	public String getIds() {
-		return ids;
-	}
-
-	@Transient
-	public void setIds(String ids) {
-		this.ids = ids;
-	}
-
-	
 	// 显示/隐藏
 	public static final String SHOW = "1";
 	public static final String HIDE = "0";
