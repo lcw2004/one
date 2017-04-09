8199c5af3b1ab8714e82d62d4693dcc078159489
李昌文 <lcw2004@163.com>
2017-04-09 10:59:52 +0800

    添加Swagger

--- one-workflow/src/main/java/com/lcw/one/workflow/config/SwaggerConfig.java
+++ one-workflow/src/main/java/com/lcw/one/workflow/config/SwaggerConfig.java
@@ -6,6 +6,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -26,10 +27,9 @@ public class SwaggerConfig {
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring Boot中使用Swagger2构建RESTful APIs")
-                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+                .title("Swagger2 RESTful APIs")
                 .termsOfServiceUrl("http://blog.didispace.com/")
-                .contact("lcw2004@163.com")
+                .contact(new Contact("lcw", "https://github.com/lcw2004", "lcw2004@163.com"))
                 .version("1.0")
                 .build();
     }
