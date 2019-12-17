41d8f68b5667beea0caee0f3b7abd728c2b0a3cb
李昌文 <lcw2004@163.com>
2019-12-17 11:30:25 +0800

    update: ReadMe.md

--- one-util/src/test/java/com/lcw/one/utils/excel/ExcelTest.java
+++ one-util/src/test/java/com/lcw/one/utils/excel/ExcelTest.java
@@ -1,7 +1,8 @@
 package com.lcw.one.utils.excel;
 
-import com.lcw.one.util.excel.ExcelExporter;
 import com.lcw.one.util.excel.ExcelUtils;
+import com.lcw.one.util.excel.rule.ExcelClassRule;
+import com.lcw.one.util.excel.rule.ExcelFieldRule;
 import com.lcw.one.util.utils.DateUtils;
 import com.lcw.one.util.utils.FileUtil;
 import com.lcw.one.utils.excel.bean.User4Excel;
@@ -10,15 +11,26 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ExcelTest {
 
     private List<User4Excel> testData() {
         List<User4Excel> userList = new ArrayList<>();
-        userList.add(new User4Excel("张三", "男", 18));
-        userList.add(new User4Excel("Lily", "女", 17));
+        userList.add(new User4Excel("张三", "男", 18, DateUtils.stringToDate("2000-01-01", DateUtils.yyyy_MM_dd_EN), "北京"));
+        userList.add(new User4Excel("Lily", "女", 17, DateUtils.stringToDate("2001-02-02", DateUtils.yyyy_MM_dd_EN), "上海"));
         return userList;
+    }
+
+    private static ExcelClassRule buildExcelRule() {
+        ExcelClassRule excelClassRule = new ExcelClassRule();
+        excelClassRule.addExcelFieldRule(new ExcelFieldRule("name", "姓名", false, String.class));
+        excelClassRule.addExcelFieldRule(new ExcelFieldRule("gender", "性别", false, String.class));
+        excelClassRule.addExcelFieldRule(new ExcelFieldRule("age", "年龄", false, Integer.class));
+        excelClassRule.addExcelFieldRule(new ExcelFieldRule("birthDay", "生日", false, Date.class, null, "yyyy-MM-dd"));
+        excelClassRule.addExcelFieldRule(new ExcelFieldRule("address", "地址", false, Integer.class));
+        return excelClassRule;
     }
 
     @Test
@@ -33,6 +45,26 @@ public class ExcelTest {
         // 导出一份数据
         List<User4Excel> userList = testData();
         byte[] bytes = ExcelUtils.saveAsExcel("测试用户", userList);
+
+        // 解析Excel文件
+        List<User4Excel> userListFromExcel = ExcelUtils.parse(User4Excel.class, new ByteArrayInputStream(bytes));
+        for (User4Excel user : userListFromExcel) {
+            System.out.println(user);
+        }
+    }
+
+    @Test
+    public void testExportUserByRule() {
+        List<User4Excel> userList = testData();
+        byte[] bytes = ExcelUtils.saveAsExcel("测试用户", userList, buildExcelRule());
+        FileUtil.writeFile(bytes, "D:\\test1.xlsx");
+    }
+
+    @Test
+    public void testImportUserByRule() throws IOException {
+        // 导出一份数据
+        List<User4Excel> userList = testData();
+        byte[] bytes = ExcelUtils.saveAsExcel("测试用户", userList, buildExcelRule());
 
         // 解析Excel文件
         List<User4Excel> userListFromExcel = ExcelUtils.parse(User4Excel.class, new ByteArrayInputStream(bytes));
--- one-util/src/test/java/com/lcw/one/utils/excel/bean/User4Excel.java
+++ one-util/src/test/java/com/lcw/one/utils/excel/bean/User4Excel.java
@@ -2,6 +2,8 @@ package com.lcw.one.utils.excel.bean;
 
 import com.lcw.one.util.annotation.ExcelField;
 
+import java.util.Date;
+
 public class User4Excel {
 
     @ExcelField(title = "姓名", column = 0, nullable = false)
@@ -13,6 +15,10 @@ public class User4Excel {
     @ExcelField(title = "年龄", column = 2)
     private Integer age;
 
+    private Date birthDay;
+
+    private String address;
+
     public User4Excel() {
     }
 
@@ -20,6 +26,14 @@ public class User4Excel {
         this.name = name;
         this.gender = gender;
         this.age = age;
+    }
+
+    public User4Excel(String name, String gender, Integer age, Date birthDay, String address) {
+        this.name = name;
+        this.gender = gender;
+        this.age = age;
+        this.birthDay = birthDay;
+        this.address = address;
     }
 
     public String getName() {
@@ -46,12 +60,30 @@ public class User4Excel {
         this.age = age;
     }
 
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
     @Override
     public String toString() {
         return "User4Excel{" +
                 "name='" + name + '\'' +
                 ", gender='" + gender + '\'' +
                 ", age=" + age +
+                ", birthDay=" + birthDay +
+                ", address='" + address + '\'' +
                 '}';
     }
 }
